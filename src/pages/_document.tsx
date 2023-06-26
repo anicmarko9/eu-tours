9d73c07f029563ed7ea944e4a2c203ad6b1ccb58
Marko Anic <anicmarko9>
2023-06-26 13:13:28 +0200

    changed body background color

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,7 @@ export default function Document() {
           rel="stylesheet"
         />
       </Head>
-      <body className="bg-[#eee]">
+      <body className="bg-bodyBg">
         <Main />
         <NextScript />
       </body>
